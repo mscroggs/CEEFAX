b4b1816e1a6851dd1317565d5e1a8164f8593614
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-12-12 06:17:12 +0000

    flake

--- ceefax/helpers/url_handler.py
+++ ceefax/helpers/url_handler.py
@@ -13,12 +13,11 @@ def load_csv(url, headers={}, cookies={}):
 
 
 def load(url, headers={}, cookies={}):
-    import urllib.request
     headers["User-Agent"] = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) "
                              "AppleWebKit/537.36 (KHTML, like Gecko) "
                              "Chrome/50.0.2661.102 Safari/537.36")
     out = requests.get(url, headers=headers, cookies=cookies).text
     try:
-        return out.decode(response.info().get_content_charset('utf-8'))
+        return out.decode('utf-8')
     except:
         return out
